5fe2e589c5bd0d19ea55e058dc5acdd8aaf18553
樊帅飞 <fanshuaifei@fanshuaifeidemacbook-pro.local>
2016-07-01 13:28:16 +0800

    新建工程

---
+++ SFLabel/SFLabel/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  SFLabel
+//
+//  Created by 樊帅飞 on 16/7/1.
+//  Copyright © 2016年 SwiftlyFly. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ SFLabel/SFLabel/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  SFLabel
+//
+//  Created by 樊帅飞 on 16/7/1.
+//  Copyright © 2016年 SwiftlyFly. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
